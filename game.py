00c1a4406ccbd23cab7c7c5fcd397411cda4d4ed
michaltkaczyk <michaltkaczyk@outlook.com>
2021-04-06 13:30:55 +0200

    Add method say_state

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 class Game:
+
     def __init__(self, finished):
         self.finished = finished
+
+    def say_state(self):
+        if self.finished:
+            print("Game finished!")
+        else:
+            print("Game in progress...")
+
+
+if __name__ == '__main__':
+    game = Game(finished=True)
+
+    game.say_state()
