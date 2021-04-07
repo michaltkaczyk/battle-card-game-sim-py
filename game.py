a6c73eedaa65fddc20de25d4904e8b4c0d31418b
michaltkaczyk <michaltkaczyk@outlook.com>
2021-04-07 17:36:23 +0200

    Shuffle cards on return to hand

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import random
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statistics import mean, median
+
+N_SIMULATIONS = 100_000
 
 
 class Deck:
@@ -18,30 +21,21 @@ class Player:
 
     def __init__(self, hand):
         self.hand = hand
-        self.stash = []
 
 
 class Game:
 
     def __init__(self):
         self.cards = Deck()
+        self.stash = []
         self.player_a = Player(self.cards[:26])
         self.player_b = Player(self.cards[26:])
         self.rounds_played = 0
         self.winner = ""
         self.finished = False
 
-    def say_state(self):
-        if self.finished:
-            print("Player", self.winner, "wins after", self.rounds_played, "rounds.")
-        else:
-            print("Game in progress,", self.rounds_played, "rounds elapsed.")
-            print("Player A's hand:", self.player_a.hand)
-            print("Player B's hand:", self.player_b.hand)
-
     def __reset_stash(self):
-        self.player_a.stash = []
-        self.player_b.stash = []
+        self.stash = []
 
     def play_round(self):
         if not self.finished:
@@ -53,19 +47,26 @@ class Game:
                 player_a_card = self.player_a.hand.pop(0)
                 player_b_card = self.player_b.hand.pop(0)
 
+                self.stash.extend([player_a_card, player_b_card])
+                random.shuffle(self.stash)
+
                 if player_a_card > player_b_card:
-                    self.player_a.hand.extend([player_a_card, player_b_card])
-                    self.player_a.hand.extend(self.player_a.stash)
-                    self.player_a.hand.extend(self.player_b.stash)
+                    self.player_a.hand.extend(self.stash)
                     self.__reset_stash()
                 elif player_b_card > player_a_card:
-                    self.player_b.hand.extend([player_b_card, player_a_card])
-                    self.player_b.hand.extend(self.player_b.stash)
-                    self.player_b.hand.extend(self.player_a.stash)
+                    self.player_b.hand.extend(self.stash)
                     self.__reset_stash()
                 else:
-                    self.player_a.stash.append(player_a_card)
-                    self.player_b.stash.append(player_b_card)
+                    if len(self.player_a.hand) == 0:
+                        self.__finish("B")
+                    elif len(self.player_b.hand) == 0:
+                        self.__finish("A")
+                    else:
+                        player_a_card = self.player_a.hand.pop(0)
+                        player_b_card = self.player_b.hand.pop(0)
+
+                        self.stash.extend([player_a_card, player_b_card])
+                        random.shuffle(self.stash)
 
                 self.rounds_played += 1
 
@@ -81,11 +82,13 @@ class Game:
 if __name__ == '__main__':
     sim_rounds_elapsed = []
 
-    for i in range(10000):
+    for i in range(N_SIMULATIONS):
         game = Game()
         game.play()
         sim_rounds_elapsed.append(game.rounds_played)
 
-    print(sim_rounds_elapsed)
+    print("Mean number of rounds:", round(mean(sim_rounds_elapsed)))
+    print("Median number of rounds:", round(median(sim_rounds_elapsed)))
+
     sns.displot(sim_rounds_elapsed, binwidth=25)
     plt.show()
