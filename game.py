0a4088600923126c26b52362f4ecb36b738f8d18
michaltkaczyk <michaltkaczyk@outlook.com>
2021-04-07 13:21:57 +0200

    Simulation layer added
    First version which actually works

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import random
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class Deck:
@@ -26,38 +28,64 @@ class Game:
         self.player_a = Player(self.cards[:26])
         self.player_b = Player(self.cards[26:])
         self.rounds_played = 0
+        self.winner = ""
         self.finished = False
 
     def say_state(self):
         if self.finished:
-            print("Game finished after", self.rounds_played, "rounds.")
+            print("Player", self.winner, "wins after", self.rounds_played, "rounds.")
         else:
             print("Game in progress,", self.rounds_played, "rounds elapsed.")
             print("Player A's hand:", self.player_a.hand)
             print("Player B's hand:", self.player_b.hand)
 
+    def __reset_stash(self):
+        self.player_a.stash = []
+        self.player_b.stash = []
+
     def play_round(self):
-        player_a_card = self.player_a.hand.pop(0)
-        player_b_card = self.player_b.hand.pop(0)
+        if not self.finished:
+            if len(self.player_a.hand) == 0:
+                self.__finish("B")
+            elif len(self.player_b.hand) == 0:
+                self.__finish("A")
+            else:
+                player_a_card = self.player_a.hand.pop(0)
+                player_b_card = self.player_b.hand.pop(0)
 
-        if player_a_card > player_b_card:
-            self.player_a.hand.extend([player_a_card, player_b_card])
-        else:
-            self.player_b.hand.extend([player_b_card, player_a_card])
+                if player_a_card > player_b_card:
+                    self.player_a.hand.extend([player_a_card, player_b_card])
+                    self.player_a.hand.extend(self.player_a.stash)
+                    self.player_a.hand.extend(self.player_b.stash)
+                    self.__reset_stash()
+                elif player_b_card > player_a_card:
+                    self.player_b.hand.extend([player_b_card, player_a_card])
+                    self.player_b.hand.extend(self.player_b.stash)
+                    self.player_b.hand.extend(self.player_a.stash)
+                    self.__reset_stash()
+                else:
+                    self.player_a.stash.append(player_a_card)
+                    self.player_b.stash.append(player_b_card)
 
-        self.rounds_played += 1
+                self.rounds_played += 1
 
-    def finish(self):
+    def __finish(self, winner):
         self.finished = True
+        self.winner = winner
+
+    def play(self):
+        while not game.finished:
+            self.play_round()
 
 
 if __name__ == '__main__':
-    game = Game()
+    sim_rounds_elapsed = []
 
-    game.say_state()
-    game.play_round()
-    game.say_state()
-    game.play_round()
-    game.say_state()
-    game.play_round()
-    game.say_state()
+    for i in range(10000):
+        game = Game()
+        game.play()
+        sim_rounds_elapsed.append(game.rounds_played)
+
+    print(sim_rounds_elapsed)
+    sns.displot(sim_rounds_elapsed, binwidth=25)
+    plt.show()
