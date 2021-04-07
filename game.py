173b32bf5e131115276afa4dade8f03122d34950
michaltkaczyk <michaltkaczyk@outlook.com>
2021-04-07 08:36:24 +0200

    Deck and Player classes added

--- game.py
+++ game.py
@@ -1,3 +1,17 @@
+import random
+
+
+class Deck:
+
+    def __init__(self):
+        deck = list(range(13)) * 4
+        random.shuffle(deck)
+        self.deck = deck
+
+    def __getitem__(self, item):
+        return self.deck[item]
+
+
 class Game:
 
     def __init__(self, finished=False, rounds_played=0):
@@ -6,22 +20,41 @@ class Game:
 
     def say_state(self):
         if self.finished:
-            print("Game finished after", self.rounds_played, "rounds")
+            print("Game finished after", self.rounds_played, "rounds.")
         else:
-            print("Game in progress after", self.rounds_played, "rounds")
+            print("Game in progress,", self.rounds_played, "rounds elapsed.")
 
-    def play_new_round(self):
+    def play_round(self):
         self.rounds_played += 1
 
     def finish(self):
         self.finished = True
 
 
-if __name__ == '__main__':
-    game = Game()
+class Player:
 
+    def __init__(self, hand, stash=None):
+        self.hand = hand
+        if stash is None:
+            self.stash = []
+
+
+if __name__ == '__main__':
+    cards = Deck()
+
+    player_a = Player(cards[:26])
+    player_b = Player(cards[26:])
+
+    print(player_a.hand)
+    player_a.hand.pop(0)
+    print(player_a.hand)
+
+    game = Game()
     game.say_state()
-    game.play_new_round()
+    game.play_round()
     game.say_state()
     game.finish()
     game.say_state()
+
+
+
